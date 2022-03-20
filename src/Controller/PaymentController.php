3863582af155baabf7bef9e7cff71f1037e01f9d
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-03-20 18:56:41 +0100

    using orderDetails instead of cart in session

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -13,30 +13,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class PaymentController extends AbstractController
 {
     #[Route('/commande/checkout/{reference}', name: 'checkout')]
-    public function payment(Cart $cart, OrderRepository $repository, $reference): Response
+    public function payment(OrderRepository $repository, $reference): Response
     {
         // Récupération des produits de la dernière commande et formattage dans un tableau pour Stripe
         $order = $repository->findOneByReference($reference);
         if (!$order) {
             throw $this->createNotFoundException('Cette commande n\'existe pas');
         }
-        
-        dd($order);
-        $products = $cart->getDetails()['products'];
+        $products = $order->getOrderDetails()->getValues();
         $productsForStripe = [];
         foreach ($products as $item) {
             $productsForStripe[] = [
                 'price_data' => [
                     'currency' => 'eur',
-                    'unit_amount' => $item['product']->getPrice(),
+                    'unit_amount' => $item->getPrice(),
                     'product_data' => [
-                        'name' => $item['product']->getName()
+                        'name' => $item->getProduct()
                     ]
                 ],
-                'quantity' => $item['quantity']
+                'quantity' => $item->getQuantity()
             ];
         }
-
+        // Ajout des frais de livraison
+        $productsForStripe[] = [
+            'price_data' => [
+                'currency' => 'eur',
+                'unit_amount' => $order->getCarrierPrice()*100,
+                'product_data' => [
+                    'name' => $order->getCarrierName()
+                ]
+            ],
+            'quantity' => 1
+        ];
         Stripe::setApiKey('sk_test_51Kb6uhClAQQ2TXfzOspWIks7VFbXX5e5ZTr5c4VCIQfNJATKvQZDHBODlaDkCnNmYntKUQLZK8YF4UbNPA5gMWzg00RHLAzE0G');
         header('Content-Type: application/json');
 
